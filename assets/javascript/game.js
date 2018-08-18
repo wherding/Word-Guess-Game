ecaea87171c4270fc8c3f682ecd20ecb00466e1e
will <wherding@asu.edu>
2018-08-17 21:50:03 -0700

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,9 @@ var spaces= [];
 var userInput;
 var userGuesses = [];
 var numberWrong = 0;
+var wins = 0;
 //begin object array
-var options = [
+var options = [                             
     oldFashioned = {  name: "old fashioned",  img:"./assets/img/mmcask.jpg"},
     marg = { name:'margarita', img:'./assets/img/patron.jpeg' },
     mint={name:"mint julep", img:"./assets/img/mintJ.jpg"},
@@ -22,8 +23,23 @@ var index;
 
 
 window.onload = function load(){
+    //var audio = new Audio('./assets/music/\Shots - LMFAO - LYRICS ON SCREEN.mp4')
+    //audio.play();
+
+    //start copy
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', './assets/music/\Shots - LMFAO - LYRICS ON SCREEN.mp4');
+    audioElement.load();
+    audioElement.play();
+    document.getElementById("win").append(audioElement);
+
+    //end copy
+ 
     console.log(options[0])
     console.log(options[1].test2)
+    numberWrong = 15;
+    document.getElementById("guesses").innerHTML = numberWrong;
+    document.getElementById("win").innerHTML = wins;
 
    // document.getElementById("test").innerHTML = words;
    index = Math.floor(Math.random() * options.length);
@@ -32,7 +48,7 @@ window.onload = function load(){
     //word = words[Math.floor(Math.random() * words.length)];
     letters = options[index].name.split("");
    // document.getElementById("test").innerHTML = letters;
-    console.log(word);
+    console.log();
     document.getElementById("hint").src = options[index].img;
   
     letters.forEach(x => {
@@ -57,35 +73,60 @@ document.onkeydown = function(event){
                spaces[i] = userInput;
                
            }
-           i++;
-           
-           
+           i++; 
        });
         //var updateSpace = letters.indexOf(userInput);
        // spaces[updateSpace] = userInput;
         document.getElementById("test").innerHTML = spaces.join(" ");
        //make this happen after the page updates the word guess line
         if(spaces.toString() == letters.toString()){
-        setTimeout(function(){
-            alert("you win!");
-            document.location.reload();
-        }, 50);
+        
           // setTimeout(1000);
-            
+          setTimeout(function(){ 
+              //begin timeout
+            alert("you win!");
+            wins++;
+            document.getElementById("win").innerHTML = wins;
             //call reset function 
-           
-        }    
+            //document.location.reload();
+            var relaod = function(){
+                index = Math.floor(Math.random() * options.length);
+                document.getElementById("hint").src = options[index].img;
+                letters = options[index].name.split("");
+               // letters = options[index].name;
+               spaces = [];
+               console.log("this is my spaces var insie of my reload b4 insterHTML: " +spaces); 
+               letters.forEach(x => {
+                    spaces.push("_");
+                });
+                
+                document.getElementById("test").innerHTML = spaces.join(" ");
+                console.log("var letters" + letters);
+                console.log("this is my spaces var insie of my reload: " +spaces);
+                userGuesses = [];
+                document.getElementById("wrong").innerHTML = userGuesses;
+                numberWrong=15;
+                document.getElementById("guesses").innerHTML = numberWrong;
+
+            };
+            relaod();
+        //end tiemout
+        }, 2000);
+        
+        }
+            
     }
     else{
         //add wrong letters to guesses array and update hanman image.
+       if(!userGuesses.includes(userInput))
+       {
         userGuesses.push(userInput);
         document.getElementById("wrong").innerHTML = userGuesses;
-        numberWrong++;
-        if(numberWrong==6){
-            alert("you lose!");
-            document.location.reload();
+        numberWrong--;
+        document.getElementById("guesses").innerHTML = numberWrong;
 
-        }
+       }
+        
 
 
         
